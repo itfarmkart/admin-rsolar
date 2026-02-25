d3e015b202f3cc63007015bba0ea19eabed88aa6
Sachin Pal <sachinpal@farmakart.com>
2026-02-25 16:50:20 +0530

    google client id change to it@farmkart.com

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import './index.css'
 import App from './App.jsx'
-
+//
 // REPLACE WITH YOUR ACTUAL GOOGLE CLIENT ID
-const GOOGLE_CLIENT_ID = "278079395226-fujjkf7eunmogp7ou2tlqmm5vl94sb4d.apps.googleusercontent.com";
+// const GOOGLE_CLIENT_ID = "278079395226-fujjkf7eunmogp7ou2tlqmm5vl94sb4d.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "822631404750-96gvhpm19qcklfldlg2b8kbofg7cdtq1.apps.googleusercontent.com";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
